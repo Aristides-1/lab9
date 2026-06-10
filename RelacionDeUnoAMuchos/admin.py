e7f62b4b39149fafd92a8ddd16df3360d7b5996e
diego <dcervantesapa@unsa.edu.pe>
2026-06-09 19:55:57 -0500

    insertando las clases de models.py para rellenar nuestros registros de las tablas de uno a muchos

--- RelacionDeUnoAMuchos/admin.py
+++ RelacionDeUnoAMuchos/admin.py
@@ -1,3 +1,25 @@
-from django.contrib import admin
+from django.db import models
 
-# Register your models here.
+class SimpleModel(models.Model):
+    text = models.CharField(max_length=10)
+    number = models.IntegerField(null=True)
+    url = models.URLField(default='https://www.google.com')
+    
+    def __str__(self):
+        return self.text
+    
+class Language(models.Model):
+    name = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.name
+
+class NullExample(models.Model):
+    col = models.CharField(max_length=10,blank=True)
+
+class Framework(models.Model):
+    name = models.CharField(max_length=15)
+    language = models.ForeignKey(Language, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
