3d744fd8acb7ef0b1bafacad55fe7a5622c9071e
diego <dcervantesapa@unsa.edu.pe>
2026-06-09 20:04:37 -0500

    corrigiendo error, el codigo de admin.py vuelve a estar vacio y se configura models.py respectivamente

--- RelacionDeUnoAMuchos/admin.py
+++ RelacionDeUnoAMuchos/admin.py
@@ -1,25 +1,3 @@
-from django.db import models
+from django.contrib import admin
 
-class SimpleModel(models.Model):
-    text = models.CharField(max_length=10)
-    number = models.IntegerField(null=True)
-    url = models.URLField(default='https://www.google.com')
-    
-    def __str__(self):
-        return self.text
-    
-class Language(models.Model):
-    name = models.CharField(max_length=15)
-
-    def __str__(self):
-        return self.name
-
-class NullExample(models.Model):
-    col = models.CharField(max_length=10,blank=True)
-
-class Framework(models.Model):
-    name = models.CharField(max_length=15)
-    language = models.ForeignKey(Language, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.name
+# Register your models here.
