c57456113d29e4db15b39035adcf01362b05b02f
diego <dcervantesapa@unsa.edu.pe>
2026-06-11 06:36:52 -0500

    mapeamos las urls de cada app para que no se confundan en un mismo puerto el http://127.0.0.1:8000/

--- relacionesunoamuchos/urls.py
+++ relacionesunoamuchos/urls.py
@@ -19,5 +19,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include ('EnvioEmails.urls')),
+
+    path('email/', include('EnvioEmails.urls')),
+
+    path('pdf/', include('GeneracionPDF.urls')),
 ]
