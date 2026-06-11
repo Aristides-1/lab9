78f50aa8f1fecfa9af311230fa9943ce37728f83
diego <dcervantesapa@unsa.edu.pe>
2026-06-11 06:28:44 -0500

    creamos la función para generar el PDF a partir de una plantilla HTML con formato JSON y datos dinámicos

--- GeneracionPDF/views.py
+++ GeneracionPDF/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from datetime import date
 
-# Create your views here.
+from relacionesunoamuchos import renderers
+
+
+def pdf_view(request):
+
+    data = {
+        "pdf_title": "Laboratorio Django",
+        "invoice_number": "001",
+        "customer_name": "Diego Cervantes",
+        "amount": "100.00",
+        "date": date.today(),
+    }
+
+    return renderers.render_to_pdf(
+        "pdfs/invoice.html",
+        data
+    )
