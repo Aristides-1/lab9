3d0affa3a4b5f7dd882b99688225fafc49d1b377
diego <dcervantesapa@unsa.edu.pe>
2026-06-11 05:01:31 -0500

    Agrega función para enviar correo electrónico desde la vista index

--- EnvioEmails/views.py
+++ EnvioEmails/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
 
 # Create your views here.
 def index(request):
+
+
+    send_mail('Hola desde Django y Python',
+              'Este es un correo de prueba y automatizado enviado desde Django. SALUDOSSS',
+              'diegozc792@gmail.com',
+              ['yetises553@fixscal.com'],
+              fail_silently=False
+              )
     return render(request, 'index.html')
